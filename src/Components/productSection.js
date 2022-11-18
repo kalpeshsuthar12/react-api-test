362c976d6b7b7f1c836b6306087369478a819cf6
kalpesh suthar <kalpesh.suthar97@gmail.com>
2022-11-18 17:20:58 +0530

    Update productSection.js

--- src/Components/productSection.js
+++ src/Components/productSection.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import axios from 'axios'
 
 export default function ProductSection() {
 
     const [amazon, setAmazon] = useState("");
     const [flipkart, setflipkart] = useState("");
+    const [allProduct, setAllProduct] = useState("");
+    const [selectedCategory, setSelectedCategory] = useState();
 
     const amazonUrl = "https://dummyjson.com/products";
     const flipkartUrl = "https://fakestoreapi.com/products";
@@ -13,7 +15,26 @@ export default function ProductSection() {
         setAmazon(amazonData.data.products)
         let flipkartData = await axios(flipkartUrl);
         setflipkart(flipkartData.data)
+        let allData = [...amazon, ...flipkart];
+        setAllProduct(allData);
     }
+
+    function getFilteredList() {
+        if (!selectedCategory) {
+          return allProduct;
+        }
+        else if(selectedCategory==="amazon"){
+            return amazon;
+        }
+        else if(selectedCategory==="flipkart"){
+            return flipkart;
+        }
+        else{
+            return allProduct;
+        }
+    }
+
+    var filteredList = useMemo(getFilteredList, [selectedCategory]);
 
     useEffect(() => {
         fetchData();
@@ -23,36 +44,17 @@ export default function ProductSection() {
         <div className="small-container">
             <div className="row row-2">
                 <h2>All Products</h2>
-            </div>
-            <div className="row row-2">
-                <h2>Amazon Products</h2>
-            </div>
-            <div className="row">
-                {
-                    Array.from(flipkart).map((item, index) => (
-                        <div className="col-4" key={index}>
-                            <img src={item.image} alt="" />
-                            <h4>{item.title}</h4>
-                            <div className="rating">
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star"></i>
-                                <i className="fas fa-star"></i>
-                            </div>
-                            <p>₹{item.price}</p>
-                        </div>
-                    ))
-                }
-            </div>
-            <div className="row row-2">
-                <h2>Flipkart Products</h2>
+                <select onChange={(e)=>setSelectedCategory(e.target.value)}>
+                    <option value="default">Default Shorting</option>
+                    <option value="amazon">Short Amazon</option>
+                    <option value="flipkart">Short Flipkart</option>
+                </select>
             </div>
             <div className="row">
                 {
-                    Array.from(amazon).map((item, index) => (
+                    Array.from(filteredList).map((item, index) => (
                         <div className="col-4" key={index}>
-                            <img src={item.images[0]} alt="" />
+                            <img src={(item.image)?item.image:item.images[0]} alt="" />
                             <h4>{item.title}</h4>
                             <div className="rating">
                                 <i className="fas fa-star"></i>
